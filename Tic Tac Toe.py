2b60dedd3b974eeffb7b5105bd8611f1506d30b7
Lukas Malleier <lukasmalleier@gmail.com>
2024-01-29 21:10:03 +0100

    local game working

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -11,78 +11,125 @@ from functools import partial
 
 def game():
     """
-    beim aufrufen immer:
-        zuerst: next()
-        danach: .send()
+    Generator Funktion des Spiels
+    zum Nutzen einmal next(<Generatorname>) ausführen, diese gibt den aktuellen Spielstand
+    für alle weiteren Züge <Generatorname>.send(<Spielzug>) aufrufen (gibt auch aktuellen Spielstand)
 
-    am Ende (wenn status 4, 5 oder 6) spiel beenden
+    Wenn der Status 4, 5 oder 6 ist, ist ein Spielende erreicht. Für erneuten Spielstart wieder mit next() beginnen.
 
     Codierung:
-        4: winner is player
-        5: tie
-        6: invalid end
+        4: Spieler in variable player hat gewonnen
+        5: unentschieden
+        6: ungültiges Ende
+    
+    yields:
+        (status: int, player: str, board.board: list[str]) : aktueller Spielstand 
     """
-    board = engine.TicTacToeBoard() # Neues Board erstellen
-    status = 0
+    # wenn der Generator nach Spielende nochmal aufgerufen wird fängt das Spiel von vorne an
+    while True:
+        board = engine.TicTacToeBoard() # Neues Board erstellen
+        if "player" in locals():    # wenn die variable player existiert
+            board.current_player = board.players[(board.players.index(player) + 1) % 2] # setze den beginnenden Spieler auf den Verlierer der Vorrunde
+        
+        status = 0  # zu Spielbeginn ist das Spiel noch in gültigem Zustand
 
-    while board.is_winner() is None and not board.is_board_full(): # Spiel geht solange es keinen Gewinner gibt und das Board nicht voll ist
-        player = board.current_player
-        yield (status, player, board.board)
+        while board.is_winner() is None and not board.is_board_full(): # Spiel geht solange es keinen Gewinner gibt und das Board nicht voll ist
+            player = board.current_player
+            chosen_position = (yield (status, player, board.board)) # Spielzug annehmen undaktuellen Spielstand zurückgeben
+            status = board.make_move(chosen_position) # Übergabe an Engine
 
-        chosen_position = (yield (status, player, board.board))
+        # Spielenden zurückgeben
+        if (winner := board.is_winner()) is not None: # Überprüfung ob es einen Gewinner gibt
+            yield (4, winner, board.board)
+        elif board.is_board_full(): # Überprüfung ob das Board voll ist -> Unentschieden
+            yield (5, player, board.board)
+        else:
+            yield (6, player, board.board)
 
-        status = board.make_move(chosen_position) # Übergabe an Engine
+def set_board(board: list[str], buttons: list):
+    """
+    setzt eingegebene buttons auf die Werte aus der Liste board
 
-    if (winner := board.is_winner()) is not None: # Überprüfung ob es einen Gewinner gibt
-        yield (4, winner, board.board)
-    elif board.is_board_full(): # Überprüfung ob das Board voll ist -> Unentschieden
-        yield (5, player, board.board)
-    yield (6, player, board)
-
-def set_board(board, buttons):
+    Parameter:
+        board: list[str] : Liste der Positionen
+        buttons: list : Liste der Buttons
+    """
     for board_spot, button_spot in zip(board, buttons):
         button_spot.setText(board_spot)
 
-def move_handler(window_game, game, move):
-    print(int(move))
-    game.send(move)
+def move_handler_local(window_game, game, move):
+    """
+    schickt Spielzüge an den Spielgenerator und setzt das Spielfeld je nach Eingabe
 
-    status, current_player, board = next(game)
-
-    print(status, current_player, board)
-
-    # print board
-    set_board(board, window_game.buttons_spots)
-
-    if status == 4:
-        winner = current_player
-        change_error_message(f"Winner: {winner}")
+    Parameter:
+        window_game : Qt-Fenster des Spielfelds
+        game : Spielgenerator
+        move : Spielzug
+    """
+    status = -1
+    try:
+        status, current_player, board = game.send(move) # Versuch Eingabe an den Generator zu schicken
+    except StopIteration:   # wenn das Ende des Generators erreicht wurde
+        return
+    
+    # Abarbeitung der Verschiedenen Spielmeldungen
+    if status == -1:
+        # Anderer Error
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "Error während des Spielzuges", None))
+    elif status == 0:
+        # Zug gültig
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", f"{current_player} ist an der Reihe", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
+    elif status == 1:
+        # Eingebe keine Ganzzahl
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", "Eingabe ist keine Ganzzahl", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
+    elif status == 2:
+        # Eingabe nicht 1-9
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", "Eingabe nicht zwischen 1-9", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
+    elif status == 3:
+        # Nicht frei
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", "Wähle einen freien Platz", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
+    elif status == 4:
+        # Gewinner
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", f"{current_player} hat gewonnen!", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
     elif status == 5:
-        # no winner, tie
-        change_error_message("tie")
+        # Unentschieden
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", "Unentschieden!", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
     elif status == 6:
-        # invalid end
-        change_error_message("invalid end")
+        # Ungültiges Ende
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", "Invalid end", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "Invalid end", None))
 
-    if status in (4, 5, 6):
-        game.close()
-        window_game.close()
-
+    # board setzten
+    set_board(board, window_game.buttons_spots)
 
 def change_error_message(message):
     window_ui.label_5.setText(f"{message}")
 
 def start_local_session():
     try:
-        game_gen = game()
+        game_gen = game()   # Generator mit der Funktion game erstellen
 
-        window_game = GUI_GAME.MainWindow()
-        for i in range(len(window_game.buttons_spots)):
-            window_game.buttons_spots[i].clicked.connect(partial(move_handler, window_game, game_gen, i))
+        window_game = GUI_GAME.MainWindow() # Spielfenster initialisieren
+        # Buttons initialisieren
+        for i in range(len(window_game.buttons_spots)): 
+            window_game.buttons_spots[i].clicked.connect(partial(move_handler_local, window_game, game_gen, i+1))
         
+        window_game.ui.pushButton_10.clicked.connect(lambda: window_game.close())
+        
+        # erster Aufruf des Spiels um aktuelle Daten zu bekommen
         status, current_player, board = next(game_gen)
         # print board
         set_board(board, window_game.buttons_spots)
+        
+        # Meldungen rücksetzen
+        window_game.ui.label_3.setText(QCoreApplication.translate("MainWindow", f"{current_player} ist an der Reihe", None))
+        window_game.ui.label_5.setText(QCoreApplication.translate("MainWindow", "", None))
 
     except Exception as e:
         change_error_message(e)
